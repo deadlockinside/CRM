07a8cb301fc66b9d0829d2ebedb984c0550142ac
deadlockinside <mytempmail228@internet.ru>
2025-08-19 20:13:17 +0300

    Create cashdesk model

---
+++ CRM.Kernel/Models/CashDesk.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRM.Kernel.Models
+{
+    public class CashDesk
+    {
+        private Context _db;
+
+        public int Number { get; set; }
+
+        public Seller Seller { get; set; }
+
+        public Queue<Cart> Queue { get; set; }
+
+        public int MaxQueueLength { get; set; }
+
+        public int ExitCustomer { get; set; }
+
+        public bool IsModel { get; set; }
+
+        public CashDesk(int number, Seller seller)
+        {
+            Number = number;
+            Seller = seller;
+            Queue = new Queue<Cart>();
+            IsModel = true;
+            _db = new Context();
+        }
+
+        public void Enqueue(Cart cart) 
+        { 
+            if (Queue.Count <= MaxQueueLength) 
+            {
+                Queue.Enqueue(cart);
+            }
+            else 
+            {
+                ExitCustomer++;
+            }
+        }
+
+        public decimal Dequeue() 
+        {
+            decimal sum = 0;
+            var cart = Queue.Dequeue();
+
+            if (cart != null) 
+            {
+                var receipt = new Receipt
+                {
+                    SellerId = Seller.Id,
+                    Seller = Seller,
+                    CustomerId = cart.Customer.Id,
+                    Customer = cart.Customer,
+                    CreatedAt = DateTime.Now
+                };
+
+                if (!IsModel) 
+                { 
+                    _db.Receipts.Add(receipt);
+                    _db.SaveChanges();
+                }
+                else 
+                {
+                    receipt.Id = 0;
+                }
+
+                var sells = new List<Sell>();
+
+                foreach(Product product in cart) 
+                {
+                    if (product.Count > 0)
+                    {
+                        var sell = new Sell
+                        {
+                            ReceiptId = receipt.Id,
+                            Receipt = receipt,
+                            ProductId = product.Id,
+                            Product = product
+                        };
+
+                        sells.Add(sell);
+
+                        if (!IsModel)
+                            _db.Sells.Add(sell);
+
+                        product.Count--;
+                        sum += product.Price;
+                    }
+                }
+
+                if (!IsModel)
+                    _db.SaveChanges();
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ CRM.KernelTests/Models/CashDeskTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CRM.Kernel.Models.Tests
+{
+    [TestClass()]
+    public class CashDeskTests
+    {
+        [TestMethod()]
+        public void CashDeskTest()
+        {
+            // arrange
+            var customer1 = new Customer 
+            {
+                Id = 1,
+                Name = "test user 1",
+            };
+
+            var customer2 = new Customer
+            {
+                Id = 2,
+                Name = "test user 2",
+            };
+
+            var seller = new Seller 
+            {
+                Id = 1,
+                Name = "test seller",
+            };
+
+            var product1 = new Product
+            {
+                Id = 1,
+                Name = "pr1",
+                Price = 100,
+                Count = 10
+            };
+
+            var product2 = new Product
+            {
+                Id = 2,
+                Name = "pr2",
+                Price = 200,
+                Count = 20
+            };
+
+            var cart1 = new Cart(customer1);
+            var cart2 = new Cart(customer2);
+            var cashdesk = new CashDesk(1, seller);
+            cashdesk.MaxQueueLength = 10;
+
+            cart1.Add(product1);
+            cart1.Add(product1);
+            cart1.Add(product2);
+
+            cart2.Add(product1);
+            cart2.Add(product2);
+            cart2.Add(product2);
+
+            cashdesk.Enqueue(cart1);
+            cashdesk.Enqueue(cart2);
+
+            var cart1ExpectedResult = 400;
+            var cart2ExpectedResult = 500;
+
+            // act
+            var cart1ActualResult = cashdesk.Dequeue();
+            var cart2ActualResult = cashdesk.Dequeue();
+
+            // assert
+            Assert.AreEqual(cart1ExpectedResult, cart1ActualResult);
+            Assert.AreEqual(cart2ExpectedResult, cart2ActualResult);
+            Assert.AreEqual(7, product1.Count);
+            Assert.AreEqual(17, product2.Count);
+        }
+    }
+}
